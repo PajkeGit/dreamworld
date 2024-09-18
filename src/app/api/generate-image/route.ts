f56866e07000d0ed64ee39d75d1dc1090f463073
PajkeGit <darth.pajke@gmail.com>
2024-09-18 14:46:20 +0200

    push

--- src/app/api/generate-image/route.ts
+++ src/app/api/generate-image/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import OpenAI from "openai";
 import Replicate from "replicate";
 
+export const runtime = "nodejs"; // add this line
+
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
